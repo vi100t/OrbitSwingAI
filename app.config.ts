4c4272f38ba1aaaf43443e8d28e084881caaa0dc
Vicente do Valle <37569763+vi100t@users.noreply.github.com>
2025-06-05 11:01:44 -0300

    Update web output configuration

--- app.config.ts
+++ app.config.ts
@@ -15,7 +15,7 @@ const config: ExpoConfig = {
   },
   web: {
     bundler: 'metro',
-    output: 'static',
+    output: 'single',
     favicon: './assets/images/favicon.png'
   },
   plugins: ['expo-router'],
